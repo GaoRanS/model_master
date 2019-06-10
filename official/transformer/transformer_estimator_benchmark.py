d3dfc2139275e9bb02c4bbc7ad671e05ea3b23d4
Igor <sheleztt@gmail.com>
2019-06-10 09:14:06 -0700

    Switch Estimator to run 600k steps.

--- official/transformer/transformer_estimator_benchmark.py
+++ official/transformer/transformer_estimator_benchmark.py
@@ -153,7 +153,7 @@ class TransformerBigEstimatorAccuracy(EstimatorBenchmark):
     FLAGS['bleu_ref'].value = self.bleu_ref
     FLAGS.param_set = 'big'
     FLAGS.batch_size = 3072 * 8
-    FLAGS.train_steps = 100000
+    FLAGS.train_steps = 600000
     FLAGS.steps_between_evals = 5000
     FLAGS.model_dir = self._get_model_dir('benchmark_graph_8_gpu')
     FLAGS.hooks = ['ExamplesPerSecondHook']
